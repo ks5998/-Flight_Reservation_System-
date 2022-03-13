932414fd041c134660ca2fb6cde56fc3e2663408
Kundan Singh <kundasin591998@outlook.com>
2022-03-13 14:39:55 +0530

    checkpoint 3

--- frs/src/com/upgrad/project/ims/Main.java
+++ frs/src/com/upgrad/project/ims/Main.java
@@ -2,43 +2,107 @@ package com.upgrad.project.ims;
 
 public class Main {
 
-    public static class Address {
-        public String street;
-        public String city;
-        public String state;
+    public static class Flight {
+        private String flightNumber;
+        private String airline;
+        private int capacity;
+        private int bookedSeats;
+        private Ticket ticket;
 
-        public void updateAddressDetails(String street, String city, String state) {
-            this.street = street;
-            this.city = city;
-            this.state = state;
+        public String getFlightNumber() {
+            return flightNumber;
         }
-    }
 
-    public static class Contact {
-        String name;
-        String phone;
-        String email;
-
-        public void updateContactDetails(String name,String phone,String email){
-            this.name= name;
-            this.phone=phone;
-            this.email=email;
+        public void setFlightNumber(String flightNumber) {
+            this.flightNumber = flightNumber;
         }
-    }
 
-    public class Flight {
-        public String flightNumber;
-        public String airline;
-        public int capacity;
-        public int bookedSeats;
+        public String getAirline() {
+            return airline;
+        }
+
+        public void setAirline(String airline) {
+            this.airline = airline;
+        }
+
+        public int getCapacity() {
+            return capacity;
+        }
+
+        public void setCapacity(int capacity) {
+            this.capacity = capacity;
+        }
+
+        public int getBookedSeats() {
+            return bookedSeats;
+        }
+
+        public void setBookedSeats(int bookedSeats) {
+            this.bookedSeats = bookedSeats;
+        }
+
+        public Ticket getTicket() {
+            return ticket;
+        }
+
+        public void setTicket(Ticket ticket) {
+            this.ticket = ticket;
+        }
 
     }
 
     public class Passenger {
         int id;
+        private static int idCounter;
+        Contact passengerContact;
+        Address passengerAddress;
 
+        public Passenger(int id, String name,String phone,String email,String street,String city,String state) {
+            idCounter++;
+            this.id = idCounter;
+            this.passengerContact = new Contact(name,phone,email);
+            this.passengerAddress = new Address(street,city,state);
+        }
+
+        int getPassengerCount(){
+            return idCounter;
+        }
+
+        private static class Contact{
+            String name;
+            String phone;
+            String email;
+
+            public Contact(String name, String phone, String email) {
+                this.name = name;
+                this.phone = phone;
+                this.email = email;
+            }
+
+            public void updateContactDetails(String name, String phone, String email){
+                this.name= name;
+                this.phone=phone;
+                this.email=email;
+            }
+        }
+        private static class Address{ public String street;
+            public String city;
+            public String state;
+
+            public Address(String street, String city, String state) {
+                this.street = street;
+                this.city = city;
+                this.state = state;
+            }
+
+            public void updateAddressDetails(String street, String city, String state){
+                this.street=street;
+                this.city=city;
+                this.state=state;
+            }
+
+        }
     }
-
     public class Ticket {
         String pnr;
         String from;
@@ -48,6 +112,28 @@ public class Main {
         String seatNumber;
         float price;
         boolean canceled;
+        private Flight flight;
+        private Passenger passenger;
+
+        public Passenger getPassenger() {
+            return passenger;
+        }
+
+        public void setPassenger(Passenger passenger) {
+            this.passenger = passenger;
+        }
+
+        public Ticket(Flight flight) {
+            this.flight = flight;
+        }
+
+        public Flight getFlight() {
+            return flight;
+        }
+
+        public void setFlight(Flight flight) {
+            this.flight = flight;
+        }
 
         public void cancel(boolean canceled){
             this.canceled=canceled;
@@ -59,6 +145,10 @@ public class Main {
 
     public class RegularTicket extends Ticket {
         String specialServices;
+
+        public RegularTicket(Flight flight) {
+            super(flight);
+        }
 
         public void updateSpecialServices(String specialServices){
             this.specialServices=specialServices;
@@ -78,25 +168,9 @@ public class Main {
 
 
         public static void main(String[] args) {
-            // write your code here
-            Address address=new Address();
-            address.street="Jalia Para";
-            address.city="Howrah";
-            address.state="West Bengal";
-
-            System.out.println(address.street);
-            System.out.println(address.city);
-            System.out.println(address.state);
-
-            Contact contact=new Contact();
-            contact.name="Kundan Singh";
-            contact.email="kundasin591998@outlook.com";
-            contact.phone="1234567890";
-
-
-            System.out.println(contact.name);
-            System.out.println(contact.email);
-            System.out.println(contact.phone);
+            Flight flight=new Flight();
+            flight.setAirline("Airbus1213");
+            System.out.println(flight.getAirline());
         }
 }
 
